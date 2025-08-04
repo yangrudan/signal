d006874322ec79ba93ef3c192d3b0fce333e2996
yangrudan <1549029765@qq.com>
2025-08-04 17:07:33 +0800

    feat: add usr2

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
 use std::time::Duration;
+use libc;
 
 // 全局原子变量，用于跨线程传递信号状态
 lazy_static! {
@@ -13,8 +14,8 @@ lazy_static! {
 }
 
 // 堆栈保存函数：在正常执行流中调用，安全执行复杂操作
-fn save_stack_trace() -> Result<(), Error> {
-    let mut file = File::create("train_stack_trace.txt")?;
+fn save_stack_trace() -> () {
+    let mut file = File::create("train_stack_trace.txt").unwrap();
     let mut backtrace = String::new();
 
     backtrace::trace(|frame| {
@@ -31,9 +32,8 @@ fn save_stack_trace() -> Result<(), Error> {
         true // 继续捕获所有帧
     });
 
-    writeln!(file, "Model training crashed. Stack trace:\n{}", backtrace)?;
+    writeln!(file, "Model training crashed. Stack trace:\n{}", backtrace).unwrap();
     eprintln!("堆栈信息已保存到 train_stack_trace.txt");
-    Ok(())
 }
 
 // 信号监听线程：低消耗轮询
@@ -58,14 +58,21 @@ fn signal_listener() {
     while !TERM_FLAG.load(Ordering::Relaxed) {
         thread::sleep(Duration::from_millis(100));
     }
+    
+    let pid = nix::unistd::getpid().as_raw(); // PID of the current process (thread group ID)
+    let tid = pid;
+    let ret = unsafe { libc::syscall(libc::SYS_tgkill, pid, tid, libc::SIGUSR2) };
+    println!("Received termination signal, saving stack trace...");
 
-    // 收到信号后保存堆栈（在正常线程中执行，安全可靠）
-    let _ = save_stack_trace();
+    // 下面拿到的 当前线程（信号处理线程） 的堆栈，而非执行 main() 函数的主线程。
+    // let _ = save_stack_trace();
 
     // 可选：执行其他清理操作（如保存模型中间状态）
     // save_checkpoint();
 
     // 退出进程（确保训练进程终止）
+    thread::sleep(Duration::from_secs(10));
+    eprintln!("训练进程已终止，堆栈信息maybe已保存。");
     std::process::exit(1);
 }
 
@@ -77,6 +84,28 @@ fn init_signal_handler() {
         signal_listener();
     });
     eprintln!("信号监听已启动，将在异常时保存堆栈信息");
+}
+
+pub fn register_signal_handler<F>(sig: std::ffi::c_int, handler: F)
+where
+    F: Fn() + Sync + Send + 'static,
+{
+    unsafe {
+        match signal_hook_registry::register_unchecked(sig, move |_: &_| handler()) {
+            Ok(_) => {
+                log::debug!("Registered signal handler for signal {sig}");
+            }
+            Err(e) => log::error!("Failed to register signal handler: {e}"),
+        }
+    };
+}
+
+#[ctor]
+fn setup() {
+    register_signal_handler(
+        nix::libc::SIGUSR2,
+        save_stack_trace,
+    );
 }
 
 
